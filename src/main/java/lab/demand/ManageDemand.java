298b3c6ee5f7d616500e00cc5e77261d2caa7414
Angello <angello.zuloaga@utec.edu.pe>
2022-03-31 22:15:17 -0500

    lab3

--- src/main/java/lab/demand/ManageDemand.java
+++ src/main/java/lab/demand/ManageDemand.java
@@ -11,18 +11,12 @@ public class ManageDemand {
     }
 
     public double calculateTotal(List<Order> orders){
-        // Calculate Taxes
+        // Calculate Taxes and Total
         double taxes = 0.0;
-        for (Order order : orders) {
-            double tax = this.tax.calculateTax(order.getCountry());
-            taxes += tax;
-        }
-
-        // Calculate Total
         double quantities = 0.0;
         for (Order order : orders) {
-            double temp = order.getQuantity();
-            quantities += temp;
+            taxes += this.tax.calculateTax(order.getCountry());
+            quantities += order.getQuantity();
         }
 
         return quantities * taxes;
@@ -32,14 +26,23 @@ public class ManageDemand {
         // Calculate additionals by country
         double taxes = 0.0;
         for (Order order : orders) {
-            String currCountry = order.getCountry();
+            double r = this.tax.calculateTax(order.getCountry());
+            if (r == 0.18) {
+                taxes += defaultAdditionalPeru;
+            } else if (r == 0.12) {
+                taxes += defaultAdditionalBrazil;
+            } else {
+                taxes += defaultAdditionalColombia;
+            }
+
+            /*String currCountry = order.getCountry();
             if (currCountry.equals("PE")) {
                 taxes += defaultAdditionalPeru;
             } else if (currCountry.equals("BR")) {
                 taxes += defaultAdditionalBrazil;
             } else {
                 taxes += defaultAdditionalColombia;
-            }
+            }*/
         }
 
         // Calculate Total
